3683983be0c15872d3f1aec24db105e3ac6f40cb
JimH <jh7@pm.me>
2019-09-02 13:58:33 -0400

    lesson 02

--- app/src/main/java/com/jim/coderswag/Model/Category.kt
+++ app/src/main/java/com/jim/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.jim.coderswag.Model
 import android.icu.text.CaseMap
 
 class Category(val title: String, val  image: String) {
+
+    override fun toString(): String {
+        return title
+    }
 }
